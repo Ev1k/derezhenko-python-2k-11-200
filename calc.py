b0e4148552b62a701d1d45a368d48c78c8080591
Evelina Derezhenko <113761562+Ev1k@users.noreply.github.com>
2023-09-12 09:00:26 +0300

    added history

--- calc.py
+++ calc.py
@@ -1,21 +1,36 @@
+plus_operations = []
+minus_operations = []
+con_operations = []
+div_operations = []
+
 while True:
     a = int(input())
     b = int(input())
     symbol = input()
 
     if symbol == "+":
-        print(str(a) + " + " + str(b) + " = " + str(a+b))
-        pass
+        res = str(a) + " + " + str(b) + " = " + str(a+b)
+        print(res)
+        plus_operations.append(res)
     elif symbol == "-":
-        print(str(a) + " - " + str(b) + " = " + str(a - b))
-        pass
+        res = str(a) + " - " + str(b) + " = " + str(a - b)
+        print(res)
+        minus_operations.append(res)
     elif symbol == "*":
-        print(str(a) + " * " + str(b) + " = " + str(a * b))
-        pass
+        res = str(a) + " * " + str(b) + " = " + str(a * b)
+        print(res)
+        con_operations.append(res)
     elif symbol == "/":
-        print(str(a) + " / " + str(b) + " = " + str(a / b))
-        pass
+        res = str(a) + " / " + str(b) + " = " + str(a / b)
+        print(res)
+        div_operations.append(res)
     else:
         print("Неверно введена операция, повторите")
+
+    print("+ " + str(plus_operations))
+    print("- " + str(minus_operations))
+    print("* " + str(con_operations))
+    print("/ " + str(div_operations))
+
 
 
