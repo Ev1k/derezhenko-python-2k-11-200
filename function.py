dc0ac810f00e0ac7c21e3ad9c8517fb8d615557a
Evelina Derezhenko <113761562+Ev1k@users.noreply.github.com>
2023-09-29 15:11:49 +0300

    function has rewritten

--- function.py
+++ function.py
@@ -1,8 +1,8 @@
 def find_max(*args):
     max_value = -1000
-    for i in range(len(args)):
-        if (max_value < int(args[i])):
-            max_value = int(args[i])
+    for item in args:
+        if (max_value < int(item)):
+            max_value = int(item)
     print(max_value)
 
 find_max(1, 2, 3, 6, 10, 0, 1000)
