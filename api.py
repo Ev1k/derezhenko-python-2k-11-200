936e68929246192c8af5f9bf1b72aee4cc5c112f
Evelina Derezhenko <113761562+Ev1k@users.noreply.github.com>
2023-10-07 23:21:55 +0300

    api has written

--- api.py
+++ api.py
@@ -5,6 +5,10 @@ import re
 ip = input()
 response = requests.get(f"http://ip-api.com/json/{ip}")
 tree = bs4.BeautifulSoup(response.text, 'html.parser')
-result = re.finditer(r"\"country\":\"([a-zA-Z-]+)\"", str(tree))
+result = re.finditer(r"(message|\"country\":\"([a-zA-Z-]+)\")", str(tree), )
+
 for item in result:
-    print(item.group(1))
+    if str(item.group(1)) == "message":
+        print("Такого IP не существует")
+        break
+    print(item.group(2))
