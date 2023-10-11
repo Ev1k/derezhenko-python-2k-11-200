ca82158846a0eb23d4c82265abc8f466de4c739a
Evelina Derezhenko <113761562+Ev1k@users.noreply.github.com>
2023-10-11 15:49:22 +0300

    parsing has rewritten + added requirements

--- api.py
+++ api.py
@@ -1,14 +1,11 @@
 import requests
-import bs4
-import re
 
 ip = input()
-response = requests.get(f"http://ip-api.com/json/{ip}")
-tree = bs4.BeautifulSoup(response.text, 'html.parser')
-result = re.finditer(r"(message|\"country\":\"([a-zA-Z-]+)\")", str(tree), )
+response = requests.post(f"http://ip-api.com/json/{ip}?fields=country")
+if not (response.json().__contains__("country")):
+    print("Такого IP не существует")
+else:
+    print(response.json()["country"])
 
-for item in result:
-    if str(item.group(1)) == "message":
-        print("Такого IP не существует")
-        break
-    print(item.group(2))
+
+
