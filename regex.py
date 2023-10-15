0a6dc4e23b1aae47f1a6c2f39a6a3f376c4858ec
Evelina Derezhenko <113761562+Ev1k@users.noreply.github.com>
2023-10-15 19:30:34 +0300

    Delete regex.py

--- regex.py
+++
@@ -1,7 +0,0 @@
-import re
-
-text = "https://www.youtube.com/watch?v=fghdgfh fhjfhhfdgsgsfg https://youtu.be/erretrt?t=1"
-result = re.finditer(r"https://(www\.youtube.com/watch\?v=|youtu\.be/)[a-zA-Z\d-]*(\?t=\d|&t=\d|)", text, re.MULTILINE)
-print()
-for item in result:
-    print(item.group(0))
